de28a2ff08bfdae42eb797167bb3fa3edc015b1f
wakakyu <wakakyu.rinkaip@gmail.com>
2025-05-10 23:43:34 +0900

    ミスの修正

--- packages/frontend/src/components/global/MkAcct.vue
+++ packages/frontend/src/components/global/MkAcct.vue
@@ -6,7 +6,7 @@ SPDX-License-Identifier: AGPL-3.0-only
 <template>
 <span>
 	<span>@{{ user.username }}</span>
-	<span v-if="!(user.username + '@' + (user.host || host)).toLowerCase().indexOf('rink@i')">@i<span style="opacity: 0.5;">{{ (user.host || host).slice(2) }}</span></span>
+	<span v-if="!(user.username + '@' + (user.host || host)).toLowerCase().indexOf('rink@i')">@i<span style="opacity: 0.5;">{{ (user.host || host).slice(1) }}</span></span>
 	<span v-else-if="user.host || detail" style="opacity: 0.5;">@{{ user.host || host }}</span>
 </span>
 </template>
